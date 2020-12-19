c793c15b8066718b1067551d2c6c36ce67064bd1
Shen Jun Wu <shenfive@gmail.com>
2020-12-19 16:10:55 +0800

    FBLogin

--- a211LoginF/AppDelegate.swift
+++ a211LoginF/AppDelegate.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+        
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        ApplicationDelegate.shared.application( application, didFinishLaunchingWithOptions: launchOptions )
+        
         return true
     }
+    
+    
+    func application( _ app:UIApplication, open url:URL, options: [UIApplication.OpenURLOptionsKey :Any] = [:] ) -> Bool {
+        ApplicationDelegate.shared.application( app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation] )
+    }
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
